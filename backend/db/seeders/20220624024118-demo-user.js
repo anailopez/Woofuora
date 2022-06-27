ce3e785abe798149c360dcd947106158360c997a
Anai Lopez <anaijocelyn@gmail.com>
2022-06-26 22:38:27 -0700

    updated icons and fixed migration error

--- backend/db/seeders/20220624024118-demo-user.js
+++ backend/db/seeders/20220624024118-demo-user.js
@@ -8,21 +8,21 @@ module.exports = {
         username: 'Goldie',
         email: 'goldie@pupmail.com',
         hashedPassword: bcrypt.hashSync('password'),
-        icon: '/media/icons/default-icon.png',
+        icon: 'https://static.wikia.nocookie.net/animalcrossing/images/8/83/Goldie_NH.png/revision/latest?cb=20200625095858',
         bio: "Hi, I'm Goldie! :)"
       },
       {
         username: 'Bones',
         email: 'bones@pupmail.com',
         hashedPassword: bcrypt.hashSync('password2'),
-        icon: '/media/icons/default-icon.png',
+        icon: 'https://static.wikia.nocookie.net/animalcrossing/images/2/2b/Bones_NH.png/revision/latest?cb=20200803113508',
         bio: 'Napping is when I do my best thinking'
       },
       {
         username: 'Cookie',
         email: 'cookie@pupmail.com',
         hashedPassword: bcrypt.hashSync('password3'),
-        icon: '/media/icons/default-icon.png',
+        icon: 'https://static.wikia.nocookie.net/animalcrossing/images/d/d9/Cookie_NH.png/revision/latest?cb=20200802135053',
         bio: "I think it's fab to show off your individuality through fashion!"
       }
     ], {});
