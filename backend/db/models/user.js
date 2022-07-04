eb463935a6ac8d8ee2defd301789628389c91dd6
Anai Lopez <anaijocelyn@gmail.com>
2022-07-04 12:54:41 -0700

    fixed bug where signup was not saving icon to the db

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -86,12 +86,14 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
 
-  User.signup = async function ({ username, email, password }) {
+  User.signup = async function ({ username, email, password, icon, bio }) {
     const hashedPassword = bcrypt.hashSync(password);
     const user = await User.create({
       username,
       email,
-      hashedPassword
+      hashedPassword,
+      icon,
+      bio
     });
     return await User.scope('currentUser').findByPk(user.id);
   };
