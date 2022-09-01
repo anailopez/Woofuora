46cff969e156b2a697a44efbf152f7f43595340e
Anai Lopez <anaijocelyn@gmail.com>
2022-08-31 22:31:25 -0700

    get all spaces functional

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,15 +3,13 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const questionsRouter = require('./questions.js');
 const answersRouter = require('./answers.js');
+const spacesRouter = require('./spaces.js');
 
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/questions', questionsRouter);
 router.use('/answers', answersRouter);
-
-// router.post('/test', function (req, res) {
-//     res.json({ requestBody: req.body });
-// });
+router.use('/spaces', spacesRouter);
 
 
 module.exports = router;
--- backend/routes/api/spaces.js
+++ backend/routes/api/spaces.js
@@ -6,8 +6,16 @@ const { Space } = require('../../db/models');
 
 
 //get all spaces
-router.get('/', asyncHandler(async (req,res) => {
-    const spaces = await Space.findAll({
-        include: ['']
-    })
-}))
+router.get('/', asyncHandler(async (req, res) => {
+    const spaces = await Space.findAll();
+    return res.json(spaces);
+}));
+
+// //get one question
+// router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
+//     const spaceId = req.params.id;
+//     const space = Space.findOne({ where: { id: spaceId } })
+//     return res.json(space);
+// }))
+
+module.exports = router;
