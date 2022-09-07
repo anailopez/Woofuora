3d53cf7fb319c4d6919a7c9231c877c48e09978a
Anai Lopez <anaijocelyn@gmail.com>
2022-09-06 19:07:19 -0700

    heroku fix attempt

--- backend/config/database.js
+++ backend/config/database.js
@@ -18,6 +18,12 @@ module.exports = {
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
-    seederStorage: 'sequelize'
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
